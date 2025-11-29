7695b938b7f35b4c19ff3f4f1f0921a6897075be
Chamiruuuu <jamesjunto22@gmail.com>
2025-11-29 10:11:24 +0800

    .

--- Models/Loans/LoanApplication.cs
+++ Models/Loans/LoanApplication.cs
@@ -8,9 +8,32 @@ namespace LendingApp.Models.Loans
 {
     public class LoanApplication
     {
-        public int ApplicationId()
-        {
-            return 0;
-        }
+     public int ApplicationId { get; set; }
+     public DateTime ApplicationDate { get; set; }
+     public decimal RequestedAmount { get; set; }
+     public int PreferredTerm { get; set; } 
+     public string Purpose { get; set; }
+     public DateTime DesireReleaseDate { get; set; }
+     public string ApplicationStatus { get; set; }
+     public string RejectionReason { get; set; }
+     public DateTime StatusDate { get; set; }
+
+
+     public void SubmitApplication()
+     {
+      // Implementation for submitting a loan application
+     }
+
+     public void UpdateStatus()
+     {
+
+     }
+
+     public void ValidateDocuments()
+     {
+       // Implementation for validating documents
+     }
+
+
     }
 }
