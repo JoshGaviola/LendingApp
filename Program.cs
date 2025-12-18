d6cee6a19bd18e85d2f308139bb79c580261ff14
Chamiruuuu <jamesjunto22@gmail.com>
2025-12-18 22:53:44 +0800

    .

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace LendingApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CustomerManagement());
+            Application.Run(new CustomerLogin());
         }
     }
 }
