2ee4c5960f396ad025ca6026d0cea9101ecb5822
JoshGaviola <martin100906@gmail.com>
2026-01-04 18:31:48 -0800

    Fixed: Visual State of the SideBar in Admin

--- UI/LoanOfficerUI/Dialog/OfficerEvaluateApplicationForm.Designer.cs
+++ UI/LoanOfficerUI/Dialog/OfficerEvaluateApplicationForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace LendingApp.UI.LoanOfficerUI.Dialog
+﻿namespace LoanApplicationUI
 {
     partial class OfficerEvaluateApplicationForm
     {
@@ -11,7 +11,7 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        
+
 
         #region Windows Form Designer generated code
 
@@ -19,9 +19,6 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
-        }
 
         #endregion
     }
